00f605075d2498fa7ce429678b7046978ce0c921
rifqirizaldi69 <kedungurang12@gmail.com>
2025-06-22 10:36:04 +0700

    Add files via upload

---
+++ latihan minggu 9/5.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body {
+            background-image: url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2YVXWtlUqmcWfi8Fp4sJj9sW7zKkBRCYZ-Q&s');
+            background-repeat: repeat;
+        }
+    </style>
+</head>
+<body>
+    
+</body>
+</html>
